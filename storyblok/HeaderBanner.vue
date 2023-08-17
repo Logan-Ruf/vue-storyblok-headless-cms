ca823cae8d023020fcf8ead66adc4e7c8f16a3cf
Logan-Ruf <cloganruf@gmail.com>
2023-08-17 12:13:19 -0700

    Added basic reactivity to banner

--- storyblok/HeaderBanner.vue
+++ storyblok/HeaderBanner.vue
@@ -5,16 +5,25 @@ interface PropTypes {
   blok: HeaderBannerStoryblok
 }
 
+const show = ref(true)
 const props = defineProps<PropTypes>()
 </script>
 
 <template>
-  <div class="flex items-center gap-x-6 bg-sand-700 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
-    <p class="text-sm leading-6 text-white">
-      <a href="#" v-html="renderRichText(props.blok.text)"></a>
-    </p>
+  <div v-if="show" class="flex items-center gap-x-6 bg-sand-700 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
+    <a href="#" class="leading-6 text-white">
+      <div v-html="renderRichText(props.blok.text)" />
+    </a>
     <div class="flex flex-1 justify-end">
-      <button type="button" class="-m-3 p-3 focus-visible:outline-offset-[-4px]">
+      <button
+        type="button"
+        class="-m-3 p-3 focus-visible:outline-offset-[-4px]"
+        @click="
+          () => {
+            show = false
+          }
+        "
+      >
         <span class="sr-only">Dismiss</span>
         <Icon name="octicon:x-24" class="h-5 w-5 text-white" aria-hidden="true" />
       </button>
